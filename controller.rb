d19c7e42a328a9fc22a826112b79770686833a36
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-21 11:36:17 +0000

    done part 5

--- controller.rb
+++ controller.rb
@@ -4,7 +4,17 @@ require( 'pry-byebug' )
 require_relative( './models/student.rb')
 also_reload('./models/*')
 
-get '/studentlist' do
+get '/hogwarts' do
   @studentlist = Student.list_all()
   erb(:list_students)
+end
+
+get '/hogwarts/new' do
+  erb(:new)
+end
+
+post '/hogwarts' do
+  @student = Student.new(params)
+  @student.save()
+  erb(:create)
 end
